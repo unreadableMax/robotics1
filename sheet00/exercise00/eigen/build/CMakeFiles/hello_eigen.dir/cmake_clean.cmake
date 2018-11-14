99067fb08d8c32fac1b144f4c0edc136ed4caf12
max <max.legnar@unreadable.eu>
2018-11-14 16:21:17 +0100

    hab sheet00 gemacht umwieder ineigen rein zu kommen

---
+++ sheet00/exercise00/eigen/build/CMakeFiles/hello_eigen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_eigen.dir/main.cpp.o"
+  "hello_eigen.pdb"
+  "hello_eigen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_eigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- sheet00/exercise00/eigen/main.cpp
+++ sheet00/exercise00/eigen/main.cpp
@@ -3,6 +3,7 @@
 #include <Eigen/Dense>
 
 using namespace Eigen;
+using namespace std;
 
 // Example for the use of eigen3 lib
 
@@ -10,23 +11,34 @@ int main(int argc, char *argv[]) {
 	
 	// Define and print a vector
 	
-	Vector3d v(1,2,3);
-	std::cout << v << std::endl;
+	Vector3d a(1,2,0);
+	std::cout <<"a= \n" <<a<< std::endl;
 	std::cout << std::endl;
 	
 	
 	// Define and print a matrix
 	
-	Matrix3d M;
-	M << 1, 0, 0,
-	     2, 5, 3,
-	     0, 2 ,8;
+	Matrix3d R;
+	R << 	0,-1,0,
+		1,0,0,
+		0,0,1;
+
+	Vector3d b = R*a;
+
+	cout << "b=R*a=\n"<<b<<endl;
+
+	cout <<"a*b=\n"<<a.dot(b)<<endl;
 	
-	std::cout << M << std::endl;
-	std::cout << std::endl;
+
+	Vector3d v;
+
+	v = a.cross(b);
+	v=v.normalized();
+
+	cout<<"normalized cross product of a and b = \n" <<v<<endl;
 	
-	// Print the product
-	std::cout << M * v<< std::endl;
+	
+
 	
 	
 	return 0;
