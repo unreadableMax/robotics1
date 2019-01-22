1c6e01f53e7775a663023b6697e1ddb3b04cad85
max <max.legnar@unreadable.eu>
2019-01-22 19:14:22 +0100

    10.3 fertig

--- sheet10/main.cpp
+++ sheet10/main.cpp
@@ -38,7 +38,8 @@ typedef std::vector<datarow_t> Data ;
 
 Vector3dVector endPoints(
 	{
-		Vector3d(0,0,1),	
+		Vector3d(0,0,.5),
+	
 		Vector3d(0,0,0),
 		Vector3d(0,1,0),
 		Vector3d(.5,2,0),
@@ -48,7 +49,8 @@ Vector3dVector endPoints(
 		Vector3d(0,0,0),
 		Vector3d(1,1,0),
 		Vector3d(1,0,0),
-		Vector3d(1,0,1),
+
+		Vector3d(1,0,.5),
 	});
 
 /* Transformation from the local to the global system */
@@ -76,15 +78,30 @@ class Transformation {
 		if (AB.norm() <1E-10 || AC.norm() < 1E-10|| n.norm() < 1E-10)
 			return false;
 		//Put your code here and replace the fixed values
-		scale = 1;
-		translate = Vector3d(0,0,0);;
+		/*
+		scale = .1;
+		translate = Vector3d(.5,0,0);;
 		Vector3d x = Vector3d(1,0,0);
 		Vector3d z = Vector3d(0,0,1);
 		Vector3d y = Vector3d(0,1,0);
+		//*/
+
+		// my code:
+		///*
+		scale = AB.norm();
+		translate = A;
+		Vector3d x = AB;
+		x.normalize();
+		Vector3d z = x.cross(AC);
+		z.normalize();
+		Vector3d y = z.cross(x);
+		y.normalize();
+		//*/
+		std::cout<<x<<std::endl<<std::endl<<y<<std::endl<<std::endl<<z<<std::endl<<std::endl;
 		
 		M <<	x[0], y[0], z[0], 
-				x[1], y[1], z[1], 
-				x[2], y[2], z[2];
+			x[1], y[1], z[1], 
+			x[2], y[2], z[2];
 		return true;
 	}
 
@@ -145,27 +162,29 @@ Vector6dVector PTP(Vector6d q_start, Vector6d q_end, double dt = 0.01, double a 
 // Put your code in this function
 Vector6dVector LIN(Model& model, Vector3d p_start, Vector3d p_end, Matrix3d ori,Vector6d q_start, double dt = 0.01, double a = .15, double vmax = .1) {
 
-	//my stuff (get "lead-achsis"):
-	
+	//------------------------------
 	Vector3d dp = p_end - p_start;
 	double longest =0;
 	for (int i =0; i < 3; i++) {
 		longest = std::max(longest, std::abs(dp[i]));
 	}
 	RampFunction rf(longest,a,vmax);
-	
+	//--------------------------------
 	
 	Vector6dVector l;
 	
-	double t_end = 1.5;
+	//double t_end = 1.5;
+	//------------------------------
+	double t_end = rf.getEndTime();
+	//---------------------------
 	Vector6d q = q_start;
 	for (double t=0; t < t_end; t+=dt) {
 		//Linear interpolation without a ramp
 		// Put your code in this function
 		//Vector3d pos = t/t_end*(p_end - p_start)+p_start;
-
+		//------------------------
 		Vector3d pos = p_start + dp*(rf.S(t)/std::abs(longest));
-		
+		//-------------------------------
 			
 		bool ret = invKin(model, pos, ori,q);
 		if (!ret) {
@@ -208,6 +227,19 @@ void appendMotion(const Vector6dVector& motion, Data& data, double& t, double dt
 
 
 int main(int argc, char *argv[]) {
+	
+	// lets see what todo by looking into argv...
+	int calibration_set=0;
+	if(argc != 2){
+		//std::cerr << "please select calibration set" << std::endl;
+		//abort();
+		calibration_set=-1;
+	}
+	else{
+		// i catch possible errors later at switch(...)...
+		calibration_set  = static_cast<int>(argv[1][0]-'0');
+		std::cout<<"calibration_set="<<calibration_set<<std::endl;
+	}
 
 	
 	Model model;
@@ -226,23 +258,64 @@ int main(int argc, char *argv[]) {
 	Transformation tr;
 
 	// Different calibrations to play with
-	
-	// assert(tr.calibrate(Vector3d(0.5,0,0),Vector3d(0.6,0.0,0),Vector3d(0.5,0.1,0)));
-	// assert(tr.calibrate(Vector3d(0.7,0,.2),Vector3d(0.7,-0.1,.2),Vector3d(0.7,0.,0.3)));
-	// assert(tr.calibrate(Vector3d(0.5,0,0.2),Vector3d(0.6,0.0,0.2),Vector3d(0.5,0.,0.3)));
-	// assert(tr.calibrate(Vector3d(0.5,0,.2),Vector3d(0.6,0.0,.2),Vector3d(0.5,0.1,0.3)));
-	
+	//----------------------------------------------
+	switch(calibration_set)
+	{
+		case 0:
+			assert(tr.calibrate(Vector3d(0.5,0,0),Vector3d(0.6,0.0,0),Vector3d(0.5,0.1,0)));
+			break;
+
+		case 1:
+			assert(tr.calibrate(Vector3d(0.7,0,.2),Vector3d(0.7,-0.1,.2),Vector3d(0.7,0.,0.3)));
+			break;
+
+		case 2:
+			assert(tr.calibrate(Vector3d(0.5,0,0.2),Vector3d(0.6,0.0,0.2),Vector3d(0.5,0.,0.3)));
+			break;
+		case 3:
+			assert(tr.calibrate(Vector3d(0.5,0,.2),Vector3d(0.6,0.0,.2),Vector3d(0.5,0.1,0.3)));
+			break;
+
+		default:
+			std::cout<<"no calibration"<<std::endl;
+	}
+	//-------------------------------------------------------
+
 	Matrix3d ori = tr.pointingMatrix();
 
 	assert(endPoints.size() > 0);
 
-	//LIN Motion	
+	//-----------------------------------------------------------------
+	// first: lets create a PTP motion from q_home to endPoints(0)
+	Vector3d p_first_draw = tr.transform(endPoints[0]);
+	Vector6d q_first_draw = q_start;
+	bool ret = invKin(model, p_first_draw, ori,q_first_draw);
+	if (!ret) 
+		std::cout << "InverseKinematics did not find a solution" << std::endl;
+	
+	Vector6dVector start_motion = PTP(q_start, q_first_draw, dt);
+	appendMotion(start_motion,data,t,dt);
+	//-----------------------------------------------------------
+
+	//LIN Motion(draw a picture)	
 	for (size_t i=0; i < endPoints.size() - 1; i++) {
 		Vector3d start = tr.transform(endPoints[i]);
 		Vector3d end = tr.transform(endPoints[(i+1)]);
 		Vector6dVector motion = LIN(model, start, end,ori, q_start, dt);
 		appendMotion(motion,data,t,dt);
 	}
+
+	//-------------------------------------------------------------
+	//move from the finished picture to home pose.
+	Vector3d p_last_draw = tr.transform(endPoints[endPoints.size() - 1]);
+	Vector6d q_last_draw=q_start;
+	ret = invKin(model, p_last_draw, ori,q_last_draw);
+	if (!ret) 
+		std::cout << "InverseKinematics did not find a solution" << std::endl;
+	
+	Vector6dVector end_motion = PTP(q_last_draw, q_start, dt);
+	appendMotion(end_motion,data,t,dt);
+	//------------------------------------------------------------------
 
 	writeFile("animation.csv", data);
 	
